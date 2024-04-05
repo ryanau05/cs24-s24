8c145bc67f93c1d0f52c7f9d30e2dbf4f5143d3f
Ryan Au <ryanaux05@gmail.com>
2024-04-04 17:41:48 -0700

    censor function working correctly

--- labs/welcome/code/censor.cpp
+++ labs/welcome/code/censor.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <cstdlib>
 #include <vector>
 using namespace std;
 
@@ -10,22 +9,41 @@ int main(int argc, char *argv[]){
 		exit(1);
 	}
 
-	int num_letters = stoi(argv[1]);
+	int word_size = stoi(argv[1]);
 	string text;
 	getline(cin, text);
 	
-	vector<string> sentence;
+	vector<string> words_split;
 	string word;
 
 	while (text.length() > 0){
-		word = text.substr(0, text.find(' '));
-		sentence.push_back(word);
+		if (text.find(' ') == string::npos){
+			word = text;
+			text = "";
+		}
 		
-		text = text.substr(text.find(' ') + 1);
+		else {
+			word = text.substr(0, text.find(' '));
+			text = text.substr(text.find(' ') + 1);
+		}
+		
+		words_split.push_back(word);
 	}
-	/*
-	for (int i = 0; i < sentence.size(); i++){
-		cout << sentence.at(i) << endl;
-	} */
+	
+	int vec_size = words_split.size();
+	int curr_size;
+	for (int i = vec_size - 1; i >= 0; i--){
+		curr_size = words_split.at(i).length();
+		if (curr_size == word_size){
+			words_split.erase(words_split.begin() + i);
+		}
+	}
+	
+	vec_size = words_split.size();
+	for (int i = 0; i < vec_size; i++){
+		cout << words_split.at(i) << " ";
+	}
+	cout << endl;
+
 	return 0;
 }
