2b6d4b85a17ffea4c0aa7c35c8abf4d33a102365
Ryan Au <ryanaux05@gmail.com>
2024-04-13 22:11:58 -0700

    testing gradescope output

--- labs/fibvec/FibVec.cpp
+++ labs/fibvec/FibVec.cpp
@@ -3,5 +3,63 @@
 // This provides exception types:
 #include <stdexcept>
 
+using namespace std;
+
 
 // FibVec Function Implementations
+
+FibVec::FibVec(){
+    delete[] mVec;
+    int mVec[fib_val(size)];
+}
+
+// returns fibonacci value
+size_t FibVec::fib_val(size_t val){
+    if (val <= 2){
+        return 1;
+    }
+    return fib_val(val - 1) + fib_val(val - 2);
+}
+
+// returns size of storage buffer
+size_t FibVec::capacity() const{
+    return 42;
+}
+// returns num items in vector
+size_t FibVec::count() const{
+    /*unsigned int i = 0;
+    while (mVec[i + 1] != NULL){
+        i++;
+    }
+    return i + 1;*/
+    return 42;
+}
+
+// inserts val at index
+void FibVec::insert(int val, size_t index){
+    /*if (index >= sizeof(*mVec)){
+        throw out_of_range("invalid index");
+        exit(1);
+    }*/
+    
+
+}
+
+// returns value at index
+int FibVec::lookup(size_t index) const{
+    /*if (index >= sizeof(*mVec) / 4){
+        throw out_of_range("invalid index");
+        exit(1);
+    }
+
+    return mVec[index];*/
+    return 42;
+}
+
+int FibVec::pop(){
+    return 42;
+}
+
+void push(int val){
+    
+}
--- labs/fibvec/FibVec.h
+++ labs/fibvec/FibVec.h
@@ -4,15 +4,30 @@
 // This provides the size_t type:
 #include <cstddef>
 
+using namespace std;
+
 class FibVec {
+
+private:
   // Member Variables
+  int mVec[0];
+  int size = 2;
 
   // Helper Functions
+  void resize(size_t size); // resizes vector to f(val)
+  size_t fib_val(size_t val); // calculates f(val)
 
 public:
   // Constructor and Destructor
+  FibVec(); // constructor
 
   // Member Functions
+  size_t capacity() const; // return size of storage buffer
+  size_t count() const; // return num of items in vector
+  void insert(int val, size_t index); // inserts val at index
+  int lookup(size_t index) const; // returns val at index
+  int pop(); // removes and returns last value of vector
+  void push(int val); // inserts val at end of vector
 
 };
 
