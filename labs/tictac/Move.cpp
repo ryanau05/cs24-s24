868fb1fcdf4ca258b8eafb46d50fec52e00d1c50
Ryan Au <ryanaux05@gmail.com>
2024-04-21 23:46:27 -0700

    retest

--- labs/tictac/Move.cpp
+++ labs/tictac/Move.cpp
@@ -2,52 +2,56 @@
 #include "Move.h"
 #include <string>
 #include <cctype>
+#include <iostream>
 
 // Space for implementing Move functions.
 
 Move::Move(const std::string& input){
     if (input.length() < 6){
-        ParseError format("Parse error");
+        ParseError format("Parse error.");
         exit(1);
     }
 
     number = input[0] - '0';
     if (!(number >= 1 && number <= 9)){
-        ParseError num("Parse error");
+        ParseError num("Parse error.");
         exit(1);
     }
 
     if (!(isspace(input[1])) || !(isspace(input[3]))){
-        ParseError whitespace("Parse error");
+        ParseError whitespace("Parse error.");
         exit(1);
     }
 
     player = input[2];
     if (player != 'X' && player != 'O'){
-        ParseError player("Parse error");
+        ParseError player("Parse error.");
         exit(1);
     }
 
     row = input[4];
     if (!((row >= 65 && row <= 67) || (row >= 97 && row <= 99))){
-        ParseError row("Parse error");
+        ParseError row("Parse error.");
         exit(1);
     }
 
 
     column = input[5] - '0';
     if (!(column >= 1 && column <= 3)){
-        ParseError column("Parse error");
+        ParseError column("Parse error.");
         exit(1);
     }
 
     int size = input.length();
     for (int i = 6; i < size; i++){
         if (input[i] != '#' || input[i - 1] != ' '){
-            ParseError comment("Parse error");
+            ParseError comment("Parse error.");
             exit(1);
         }
     }
+
+    std::cout << to_string();
+    exit(0);
 }
 
 std::string Move::to_string() const{
