ff93a8406dafc7e9f16edde97a76efb53e6e268a
Ryan Au <ryanaux05@gmail.com>
2024-04-21 20:41:00 -0700

    test

--- labs/fibvec/test.cpp
+++ labs/fibvec/test.cpp
@@ -7,7 +7,26 @@
 int main() {
   FibVec fibvec;
 
-  std::cout << "This program doesn't do anything interesting yet...\n";
+  /*fibvec.insert(90, 0);
+  fibvec.insert(80, 1);
+  fibvec.insert(70, 2);
+  fibvec.insert(60, 3);
+  fibvec.insert(50, 4);
+  fibvec.insert(40, 5);
+  fibvec.insert(30, 6);
+  fibvec.insert(20, 7);
+  fibvec.insert(10, 8);
+
+  int x = fibvec.count();
+
+  cout << fibvec.remove(4) << endl;
+  for (int i = 0; i < x - 1; i++){
+    cout << fibvec.lookup(i) << " ";
+  }
+  cout << endl;*/
+  fibvec.push(3);
+  cout << fibvec.lookup(0);
+
 
   return 0;
 }
