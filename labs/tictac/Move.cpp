0a331a68f26a1025e8223e8c410c8f3195c49c28
Ryan Au <ryanaux05@gmail.com>
2024-04-22 16:51:14 -0700

    test for heap?

--- labs/tictac/Move.cpp
+++ labs/tictac/Move.cpp
@@ -16,9 +16,9 @@ Move::Move(const std::string& input){
     }
 
     if (num < 4){
-        //std::cout << "Parse error." << std::endl;
-        ParseError size("Parse error.");
-        throw size;
+        std::cout << "Parse error." << std::endl;
+        //ParseError size("Parse error.");
+        //throw size;
         exit(1);
     }
 
@@ -30,9 +30,9 @@ Move::Move(const std::string& input){
             if (count == 0){
                 if (!(input[i] - '0' >= 1 && input[i] - '0' <= 9) || isspace(input[i - 1])){
                 //if (isspace(input[i - 1])){
-                    //std::cout << "Parse error." << std::endl;
-                    ParseError number("Parse error");
-                    throw number;
+                    std::cout << "Parse error." << std::endl;
+                    //ParseError number("Parse error");
+                    //throw number;
                     exit(1);
                 }
                 else {
@@ -42,9 +42,9 @@ Move::Move(const std::string& input){
             }
             else if (count == 1){
                 if ((input[i] != 'X' && input[i] != 'x' && input[i] != 'O' && input[i] != 'o') || !(isspace(input[i - 1]))){
-                    //std::cout << "Parse error." << std::endl;
-                    ParseError player("Parse error");
-                    throw player;
+                    std::cout << "Parse error." << std::endl;
+                    //ParseError player("Parse error");
+                    //throw player;
                     exit(1);
                 }
                 else {
@@ -54,9 +54,9 @@ Move::Move(const std::string& input){
             }
             else if (count == 2){
                 if (!((input[i] >= 65 && input[i] <= 67) || (input[i] >= 97 && input[i] <= 99)) || !(isspace(input[i - 1]))){
-                    //std::cout << "Parse error." << std::endl;
-                    ParseError row("Parse error");
-                    throw row;
+                    std::cout << "Parse error." << std::endl;
+                    //ParseError row("Parse error");
+                    //throw row;
                     exit(1);
                 }
                 else {
@@ -66,9 +66,9 @@ Move::Move(const std::string& input){
             }
             else if (count == 3){
                 if (!(input[i] - '0' >= 1 && input[i] - '0' <= 3) || (input[i - 1] != row)){
-                    //std::cout << "Parse error." << std::endl;
-                    ParseError column("Parse error");
-                    throw column;
+                    std::cout << "Parse error." << std::endl;
+                    //ParseError column("Parse error");
+                    //throw column;
                     exit(1);
                 }
                 else {
@@ -78,9 +78,9 @@ Move::Move(const std::string& input){
             }
             else if (count == 4){
                 if (!(input[i] == '#' && isspace(input[i - 1]))){
-                    //std::cout << "Parse error." << std::endl;
-                    ParseError comment("Parse error");
-                    throw comment;
+                    std::cout << "Parse error." << std::endl;
+                    //ParseError comment("Parse error");
+                    //throw comment;
                     exit(1);
                 }
                 else {
