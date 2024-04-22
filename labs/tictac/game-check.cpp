50914e834c12eba47bf62b9aab224c3ed5b43163
Ryan Au <ryanaux05@gmail.com>
2024-04-21 23:36:16 -0700

    fixed comments for parser

--- labs/tictac/game-check.cpp
+++ labs/tictac/game-check.cpp
@@ -5,5 +5,16 @@
 #include <string>
 
 int main() {
+    std::string text;
 
+    std::getline(std::cin, text);
+    
+    Move move(text);
+
+    std::string a = move.to_string();
+
+    std::cout << a;
+    
+
+    return 0;
 }
