4541e651e3c122e503a1e70a130765fb0fdd252d
Ryan Au <ryanaux05@gmail.com>
2024-04-30 23:03:39 -0700

    preliminary test

--- labs/rotate/Tree.cpp
+++ labs/rotate/Tree.cpp
@@ -1,3 +1,105 @@
 #include "Tree.h"
-
+#include <iostream>
 // Tree Function Implementations
+Tree::Tree(){
+    root = new Node;
+    root = nullptr;
+}
+
+Tree::~Tree(){
+    delete root;
+}
+
+void Tree::clearAll(Node* node){
+    if (node == nullptr){
+        return;
+    }
+    clearAll(node->left);
+    clearAll(node->right);
+    
+    delete node;
+}
+
+void Tree::clear(){
+    clearAll(root);
+    nodeCount = 0;
+}
+
+size_t Tree::count() const{
+    return nodeCount;
+}
+
+bool Tree::contains(const std::string& s) const{
+    if (root == nullptr){
+        return false;
+    }
+
+    Node* curr = root;
+    while (s != curr->data){
+        if ((s < curr->data && curr->left == nullptr) || (s > curr->data && curr->right == nullptr)){
+            return false;
+        }
+        else if (s < curr->data){
+            curr = curr->left;
+        }
+        else if (s > curr->data){
+            curr = curr->right;
+        }
+
+    }
+    return true;
+}
+
+void Tree::insert(const std::string& s){
+    Node* add = new Node;
+    add->data = s;
+    
+    if (root == nullptr){
+        root = add;
+        nodeCount += 1;
+        return;
+    }
+
+    Node* curr = root;
+    while (curr->left != nullptr && curr->right != nullptr){
+        if (s <= curr->data){
+            curr = curr->left;
+        }
+        else if (s > curr->data){
+            curr = curr->right;
+        }
+    }
+    if (s <= curr->data){
+        curr->left = add;
+    }
+    else {
+        curr->right = add;
+    }
+
+    add->parent = curr; 
+    nodeCount += 1;
+
+    while (curr != root){
+        curr->weight += 1;
+        curr = curr->parent;
+    }
+    root->weight += 1;
+}
+
+void Tree::print() const{
+    print_all(root);
+    std::cout << std::endl;
+}
+
+void Tree::print_all(const Node* node) const{
+    if (node == nullptr) {
+        std::cout << "-";
+    } 
+    else {
+        std::cout << "(";
+        print_all(node->left);
+        std::cout << " " << node->data << " ";
+        print_all(node->right);
+        std::cout << ")";
+    }
+}
--- labs/rotate/Tree.h
+++ labs/rotate/Tree.h
@@ -5,19 +5,24 @@
 
 class Tree {
   // Member Variables
-
+  Node* root;
+  size_t nodeCount = 0;
   // Private Helper Functions
+  void rebalance(Node* promote);
+  void clearAll(Node* curr);
+  void print_all(const Node* node) const;
+
 
 public:
   Tree();
   ~Tree();
 
   // Public Member Functions
-  void        clear();
-  size_t      count() const;
-  bool        contains(const std::string& s) const;
+  void        clear();                              // done
+  size_t      count() const;                        // done
+  bool        contains(const std::string& s) const; // done
   size_t      find(const std::string& s) const;
-  void        insert(const std::string& s);
+  void        insert(const std::string& s);         // done
   std::string lookup(size_t index) const;
   void        print() const;
   void        remove(size_t index);
