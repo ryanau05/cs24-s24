d6744b85aa7ef8ac6d8a66f9fb4b407bb18ed8ef
Ryan Au <ryanau@ucsb.edu>
2024-05-10 17:19:07 -0700

    a

--- labs/polish/Nodes.cpp
+++ labs/polish/Nodes.cpp
@@ -21,11 +21,11 @@ node::node(std::string data_, std::string type_){
 node::~node(){
   if (left != nullptr){
     delete left;
-    left == nullptr;
+    left = nullptr;
   }
   if (right != nullptr){
     delete right;
-    right == nullptr;
+    right = nullptr;
   }
 }
 
