1e4218f23ef783ab8002ac990da853e782ce0e7a
Ryan Au <ryanau@ucsb.edu>
2024-05-26 22:29:02 -0700

    test

--- labs/typo/WordList.cpp
+++ labs/typo/WordList.cpp
@@ -1,4 +1,5 @@
 #include "WordList.h"
+#include "Point.h"
 #include <iostream>
 #include <cctype>
 #include <cmath>
@@ -21,33 +22,37 @@ WordList::WordList(std::istream& stream) {
 
 Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const {
     Heap heap(maxcount);
-    
-    for (const auto& word : mWords) {
-        if (word.size() == points.size()) {
-            float totalScore = 0.0f;
-            
-            for (size_t i = 0; i < word.size(); ++i) {
-                if (word[i] >= 'A' && word[i] <= 'Z') {
-                    float dx = points[i].x - QWERTY[word[i] - 'A'].x;
-                    float dy = points[i].y - QWERTY[word[i] - 'A'].y;
-                    float distance = std::sqrt(dx * dx + dy * dy);
-                    
-                    float score = 1.0f / (10.0f * distance * distance + 1.0f);
-                    
-                    totalScore += score;
-                }
+    for (auto& word: mWords){
+        if (word.size() == points.size()){
+            float px, py, qx, qy;
+            float dist;
+            float letterScore;
+            float totalScore = 0;
+            float avgScore;
+
+            for (size_t i = 0; i < points.size(); i++){
+                px = points.at(i).x;
+                py = points.at(i).y;
+                qx = QWERTY[word[i] - 'a'].x;
+                qy = QWERTY[word[i] - 'a'].y;
+                dist = sqrt(pow((px - qx), 2) + pow((py - qy), 2));
+                letterScore = 1 / ((10 * pow(dist, 2)) + 1);
+                totalScore += letterScore;
             }
-            
-            float avgScore = totalScore / static_cast<float>(word.size());
-            
-            if (avgScore >= cutoff) {
-                try {
-                    heap.push(word, avgScore);
-                } catch (const std::overflow_error&) {
-                }
+
+            avgScore = totalScore / points.size();
+            if (heap.count() == maxcount){
+                heap.pushpop(word, avgScore);
+            }
+            else {
+                heap.push(word, avgScore);
             }
         }
     }
-    
+    for (size_t i = 0; i < heap.count(); i++){
+        if (heap.lookup(i).score < cutoff){
+            heap.pop();
+        }
+    }
     return heap;
 }
