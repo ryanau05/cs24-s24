c58a10d186c6cc2b038c0c81f9750f3bbf03c96b
Ryan Au <ryanau@ucsb.edu>
2024-06-14 06:44:09 -0700

    test

--- labs/voxhop/VoxMap.cpp
+++ labs/voxhop/VoxMap.cpp
@@ -98,11 +98,16 @@ std::string VoxMap::hexToBin(char val) const{
   std::map<char, std::string> hexmap = {
         {'0', "0000"}, {'1', "0001"}, {'2', "0010"}, {'3', "0011"},
         {'4', "0100"}, {'5', "0101"}, {'6', "0110"}, {'7', "0111"},
-        {'8', "1000"}, {'9', "1001"}, {'a', "1010"}, {'b', "1011"}, 
+        {'8', "1000"}, {'9', "1001"}, {'a', "1010"}, {'b', "1011"},
         {'c', "1100"}, {'d', "1101"}, {'e', "1110"}, {'f', "1111"}
-  };
+    };
 
-  return hexmap[val];
+    auto it = hexmap.lower_bound(val);
+    std::string hexBin = "";
+    if (it != hexmap.end()) {
+        hexBin = it->second;
+    }
+    return hexBin;
 }
 
 int VoxMap::heuristic(const Point& a, const Point& b){
